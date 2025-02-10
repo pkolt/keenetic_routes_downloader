ce5b2dc0f3c42f9532ebdb8ed46c4934e68357d1
pkolt <pkoltyshev@gmail.com>
2025-02-10 15:05:55 +0500

    Fixed icon status

--- src/service_worker.ts
+++ src/service_worker.ts
@@ -1,55 +1,73 @@
 import { getRoutesFromConfig, convertRoutesToText, sortRoutes } from './utils.js';
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete' && tab.url) {
-    if (tab.url.startsWith('http://192.168.1.1')) {
-      chrome.action.setIcon({ path: 'icon_enabled.png', tabId: tabId });
-    } else {
-      chrome.action.setIcon({ path: 'icon_disabled.png', tabId: tabId });
+const BASE_URL = 'http://192.168.1.1';
+const TAB_URL = `${BASE_URL}/`;
+const LOGIN_URL = `${BASE_URL}/login`;
+const CONFIG_URL = `${BASE_URL}/ci/startup-config.txt`;
+
+chrome.webNavigation.onCompleted.addListener(
+  ({ url, tabId }) => {
+    updateIcon(tabId, url);
+  },
+  { url: [{ urlPrefix: TAB_URL }] },
+);
+
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+  ({ tabId, url }) => {
+    updateIcon(tabId, url);
+  },
+  { url: [{ urlPrefix: TAB_URL }] },
+);
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, ({ id, url }) => {
+    if (id && url) {
+      updateIcon(id, url);
     }
-  }
+  });
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url && tab.id && tab.url.startsWith('http://192.168.1.1')) {
-    const configUrl = 'http://192.168.1.1/ci/startup-config.txt';
-    let configText = '';
-
+  if (tab.url && tab.id && userHasAuthorization(tab.url)) {
     try {
-      const response = await fetch(configUrl);
-      configText = await response.text();
-    } catch (error) {
-      console.error(error);
-      alert('Не удалось загрузить файл startup-config.txt');
-    }
-
-    let routesText = '';
-
-    try {
+      const configText = await getConfigText();
       const routes = getRoutesFromConfig(configText);
       const orderedRoutes = sortRoutes(routes);
-      routesText = convertRoutesToText(orderedRoutes);
-    } catch (error) {
-      console.error(error);
-      alert('Не удалось разобрать файл startup-config.txt');
-    }
-
-    try {
-      const date = new Date();
-      const day = `${date.getDate()}`.padStart(2, '0');
-      const month = `${date.getMonth() + 1}`.padStart(2, '0');
-      const year = `${date.getFullYear()}`;
-      const minutes = `${date.getMinutes()}`.padStart(2, '0');
-      const hours = `${date.getHours()}`.padStart(2, '0');
-      const dateStr = `${day}_${month}_${year}_${hours}_${minutes}`;
-      const filename = `routes_${dateStr}.txt`;
+      const routesText = convertRoutesToText(orderedRoutes);
+      const filename = makeRoutesFilename();
       await downloadFile(routesText, filename);
     } catch (error) {
       console.error(error);
-      alert('Не удалось выгрузить файл с маршрутами');
+      alert('Ошибка при выгрузке файла с маршрутами');
     }
   }
 });
+
+function userHasAuthorization(url: string) {
+  return url.startsWith(TAB_URL) && !url.startsWith(LOGIN_URL);
+}
+
+function updateIcon(tabId: number, url: string) {
+  const iconPath = userHasAuthorization(url) ? 'icon_enabled.png' : 'icon_disabled.png';
+  chrome.action.setIcon({ path: iconPath, tabId: tabId });
+}
+
+function makeRoutesFilename(): string {
+  const date = new Date();
+  const day = `${date.getDate()}`.padStart(2, '0');
+  const month = `${date.getMonth() + 1}`.padStart(2, '0');
+  const year = `${date.getFullYear()}`;
+  const minutes = `${date.getMinutes()}`.padStart(2, '0');
+  const hours = `${date.getHours()}`.padStart(2, '0');
+  const dateStr = `${day}_${month}_${year}_${hours}_${minutes}`;
+  return `routes_${dateStr}.txt`;
+}
+
+async function getConfigText(): Promise<string> {
+  const response = await fetch(CONFIG_URL);
+  const configText = await response.text();
+  return configText;
+}
 
 async function downloadFile(content: string, filename: string): Promise<void> {
   return new Promise((resolve, reject) => {
